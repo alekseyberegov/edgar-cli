36f06941786962b1d195cf30e042498515e589de
clicktripz <l87dg5f1!>
2021-05-02 20:51:30 -0700

    Little refactoring

--- scripts/commands/index.sh
+++ scripts/commands/index.sh
@@ -21,12 +21,20 @@ function usage()
     exit 1
 }
 
-function calc_index()
+function set_params()
 {
+    param_date=${param_date:-$default_workday}
+    param_sort=${param_sort:-$default_sort}
+    param_index=${param_index:-$default_index}
     param_year=$(($param_date / 10000))
     param_month=$(($param_date / 100 - $param_year * 100))
     param_quarter=$((($param_month - 1) / 3 + 1 ))
     param_file=$([ "${param_index}" == "daily" ] && echo "${param_sort}.${param_date}" || echo "${param_sort}" )
+}
+
+function create_cache_key()
+{
+    echo "${param_year}-Q${param_quarter}-${param_file}-${param_index}"
 }
 
 function parse_args()
@@ -58,16 +66,11 @@ function parse_args()
     done
 }
 
-param_date="20210401"
-param_sort="master"
-param_index="daily"
-
 parse_args "$@"
-calc_index
+set_params
 
 index_uri="${edgar_archive_url}/edgar/${param_index}-index/${param_year}/QTR${param_quarter}/${param_file}.idx"
-cache_key="${param_year}-Q${param_quarter}-${param_file}-${param_index}"
-cache_file=$(get_cache_file "${cache_key}")
+cache_file=$(get_cache_file "$(create_cache_key)")
 
 if [[ ! -f ${cache_file} ]]
 then
